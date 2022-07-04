1f6c294cd5d0677a7832780d195ca81c52f09dae
Vishnu <vishnusr01@gmail.com>
2022-07-04 16:05:25 +0800

    Style nav bar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons';
 import './navbar.css';
+import logo from '../../assets/logo.png';
 
 const NavBar = () => {
     return (
-        <div>
-            NavBar
+        <div className="navbar">
+            <div className="navbar-links">
+                <div className="navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+
+                <div className='navbar-links_container'>
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#what">What is UI UX</a></p>
+                    <p><a href="#possibility">Open AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
         </div>
     )
 }
