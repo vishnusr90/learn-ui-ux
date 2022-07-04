c420cfd06767bbc1816c0720be6328486f25320f
Vishnu <vishnusr01@gmail.com>
2022-07-04 10:51:53 +0800

    Add containers and export files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import Article from './componenets/article/Article'
-import Brand from './componenets/brand/Brand'
-import CTA from './componenets/cta/CTA'
-import feature from './componenets/feature/Feature'
-import NavBar from './componenets/navbar/NavBar'
+import { Brand, CTA, NavBar } from './componenets'
+import { Header, Footer, Blog, Possibility, Features } from './containers';
 
 const App = () => {
     return (
