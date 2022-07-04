027a133ceadf66bc14ac3d4b38ce2db98501e0d4
Vishnu <vishnusr01@gmail.com>
2022-07-04 10:39:10 +0800

    Add src folder with components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Article from './componenets/article/Article'
+import Brand from './componenets/brand/Brand'
+import CTA from './componenets/cta/CTA'
+import feature from './componenets/feature/Feature'
+import NavBar from './componenets/navbar/NavBar'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Learn UI UX</h1>
+        </div>
+    )
+}
+
+export default App
